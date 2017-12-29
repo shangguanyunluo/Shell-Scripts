ca02b8c13355e565fbe235425cdd21865ceae281
shangguanyunluo <31437644+shangguanyunluo@users.noreply.github.com>
2017-12-29 15:10:11 +0800

    Create clear_scripts.sh
    execute clear.sh in remote server

---
+++ ssh-shell/clear_scripts.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+clear(){
+
+server_ip_list=('192.168.0.25' '192.168.0.27' '192.168.0.28' '192.168.0.30')
+
+echo "server list is : "${server_ip_list[@]}
+
+cmd_install="yum install -y sshpass"
+cmd_clear="sh /etc/ceph/scripts/clear.sh"
+cmd_stopctdb="systemctl stop ctdb"
+cmd_ctdb="ps -ef | grep ctdb | awk '{print \$2}'| xargs -I {} kill -9 {}"
+cmd_mount="mount | grep f2fs | awk '{print \$3}'"
+cmd_umount="mount | grep nfs4 | awk '{cmd=\"umount \"\$3;system(cmd)}'"
+var_sshpass=$(rpm -qa sshpass)
+
+if [ -z $var_sshpass ]
+then
+    ${cmd_install}
+else
+    echo "sshpass has installed."
+fi
+
+for server_ip in ${server_ip_list[@]}
+do
+    # sh clear.sh
+    echo "--------------------Start execute clear.sh--------------"
+    sshpass -p lenovo ssh -tt -o "StrictHostKeyChecking no" root@${server_ip} <<EOF
+	${cmd_stopctdb}
+	${cmd_ctdb}
+	${cmd_clear}
+	exit
+EOF
+    echo "-------------------Finished to execute clear.sh------------------"
+
+    cmd_mount='mount | grep f2fs | grep -o -E "/Ceph(/\w+)+(-\w+)+"'
+    var=$(sshpass -p lenovo ssh -tt -o 'StrictHostKeyChecking no'  root@${server_ip} ${cmd_mount})
+
+
+    if [ -z ${var[@]} ]
+    then
+        echo "There is nothing to clear."
+    else
+        echo '------------------------------'
+        for i in $var
+        do
+            cmd_umount="umount $i"
+            sshpass -p lenovo ssh -o 'StrictHostKeyChecking no' root@${server_ip} ${cmd_umount}
+        done
+        echo '===================='
+    fi
+done
+}
+
+clear
+
+
