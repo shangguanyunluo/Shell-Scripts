08ec781422967879ea59a9a248dab4a7a1c5f384
zhangyafeng <cloudtester2@lenovo.com>
2018-02-05 14:51:23 +0800

    some update: add login method
    Signed-off-by: zhangyafeng <cloudtester2@lenovo.com>

--- nova_api_test/nova_api_test.py
+++ nova_api_test/nova_api_test.py
@@ -4,6 +4,8 @@ import logging
 import time
 
 import sys
+
+import aexpect
 from exceptions import Exception
 import openstack_utils
 
@@ -13,6 +15,7 @@ flavor_name = "yardstick-pktgen-dpdk.flavor"
 key_name = "zyf_key"
 network_name = "cloudtest_net"
 security_groups = ["28fb1da7-f040-4edb-b435-63795eabc976"]
+username = 'root'
 
 
 def create_vm(image_name=image_name, vm_name=vm_name, flavorname=flavor_name,
@@ -48,6 +51,35 @@ def assign_floating_ip_to_vm(vm_name=vm_name, fip_list=None):
         vm.add_floating_ip(free_ip)
         print("Assigned floating IP '%s' to VM '%s'" % (free_ip, vm.name))
         return free_ip
+    return assigned_ips[0]
+
+
+def login_vm(vm_ip=None, username=username, password=None, timeout=360):
+    print("Prepare to login vm:%s." % vm_ip)
+    cmd = 'whoami'
+    use_key = password is None
+    end_time = time.time() + timeout
+    responsive = False
+    while time.time() < end_time:
+        if responsive:
+            return True
+        try:
+            session = openstack_utils.RemoteRunner(host=vm_ip,
+                                                   username=username,
+                                                   password=password,
+                                                   use_key=use_key,
+                                                   timeout=timeout)
+            if not session:
+                continue
+            result = session.run(cmd)
+            print("%s output is : %s" % (cmd, result))
+            if username in result.stdout:
+                responsive = True
+        except Exception, e:
+            print('Failed to login vm: %s' % e)
+            continue
+    print("Success to login vm:%s." % vm_ip)
+    return responsive
 
 
 def find_vm_from_name(vm_name=None):
@@ -69,6 +101,7 @@ def del_vm(name=vm_name):
 
 
 def stop_vm(name=vm_name):
+    print("Prepare to %s vm:%s" % ("stop", name))
     _vm = find_vm_from_name(name)
     _vm.stop()
     if wait_for_vm_in_status(_vm, 'SHUTOFF'):
@@ -78,6 +111,7 @@ def stop_vm(name=vm_name):
 
 
 def start_vm(name=vm_name):
+    print("Prepare to %s vm:%s" % ("start", name))
     _vm = find_vm_from_name(name)
     _vm.start()
     if wait_for_vm_in_status(_vm, 'ACTIVE'):
@@ -111,22 +145,37 @@ def wait_for_vm_in_status(vm, status, step=3, timeout=360,
         return False
 
 
+def operations_of_vm():
+    create_vm()
+    time.sleep(2)
+    ip = assign_floating_ip_to_vm()
+    time.sleep(2)
+    login_vm(vm_ip=ip)
+    time.sleep(2)
+    stop_vm()
+    time.sleep(5)
+    start_vm()
+    time.sleep(5)
+    del_vm()
+
+
 def usage():
     _doc_ = """
     -c   --create : create a vm;
     -s   --start  : start a vm which is stoped;
     -d   --delete : delete a vm;
     -a   --add_fip: add floating ip to vm;
-    --stop        : stop a vm which is running.
+    -S   --stop   : stop a vm which is running;
+    -A   --AllOperation : all operations about vm.
     """
     print(_doc_)
 
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "csda",
+        opts, args = getopt.getopt(argv, "csdaSAl:",
                                    ["create", "start", "delete", "add_fip",
-                                    "stop"])
+                                    "stop", "AllOperation"])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -137,12 +186,17 @@ def main(argv):
             method = create_vm
         elif opt in ("-s", '--start'):
             method = start_vm
-        elif opt == '--stop':
+        elif opt in ('-S', '--stop'):
             method = stop_vm
         elif opt in ("-d", "--delete"):
             method = del_vm
         elif opt in ("-a", "--add_fip"):
             method = assign_floating_ip_to_vm
+        elif opt in ("-A", "--AllOperation"):
+            method = operations_of_vm
+        elif opt == '-l':
+            login_vm(vm_ip=arg)
+            sys.exit(0)
 
     method()
 
--- nova_api_test/openstack_utils.py
+++ nova_api_test/openstack_utils.py
@@ -10,9 +10,12 @@
 from __future__ import absolute_import
 
 import os
+import random
 import time
 import logging
 
+import aexpect
+import math
 from keystoneauth1 import loading
 from keystoneauth1 import session
 from cinderclient import client as cinderclient
@@ -84,7 +87,7 @@ def get_credentials():
 
 def get_session_auth():
     loader = loading.get_plugin_loader('password')
-    set_openstack_environment()
+    # set_openstack_environment()
     creds = get_credentials()
     auth = loader.load_from_options(**creds)
     return auth
@@ -559,3 +562,351 @@ def create_floating_ip(network_id=None):
     print("Successfully created floating ip: %s"
           % response_fip['floatingip']['floating_ip_address'])
     return response_fip
+
+
+# *********************************************
+#   LOGIN VM
+# *********************************************
+def handle_prompts(session, username, password, prompt, timeout=10,
+                   debug=False):
+    """
+    Connect to a remote host (guest) using SSH or Telnet or else.
+
+    Wait for questions and provide answers.  If timeout expires while
+    waiting for output from the child (e.g. a password prompt or
+    a shell prompt) -- fail.
+
+    :param session: An Expect or ShellSession instance to operate on
+    :param username: The username to send in reply to a login prompt
+    :param password: The password to send in reply to a password prompt
+    :param prompt: The shell prompt that indicates a successful login
+    :param timeout: The maximal time duration (in seconds) to wait for each
+            step of the login procedure (i.e. the "Are you sure" prompt, the
+            password prompt, the shell prompt, etc)
+    :raise LoginTimeoutError: If timeout expires
+    :raise LoginAuthenticationError: If authentication fails
+    :raise LoginProcessTerminatedError: If the client terminates during login
+    :raise LoginError: If some other error occurs
+    :return: If connect succeed return the output text to script for further
+             debug.
+    """
+    password_prompt_count = 0
+    login_prompt_count = 0
+
+    output = ""
+    while True:
+        try:
+            match, text = session.read_until_last_line_matches(
+                [r"[Aa]re you sure", r"[Pp]assword:\s*",
+                 # Prompt of rescue mode for Red Hat.
+                 r"\(or (press|type) Control-D to continue\):\s*$",
+                 r"[Gg]ive.*[Ll]ogin:\s*$",  # Prompt of rescue mode for SUSE.
+                 r"(?<![Ll]ast )[Ll]ogin:\s*$",  # Don't match "Last Login:"
+                 r"[Cc]onnection.*closed", r"[Cc]onnection.*refused",
+                 r"[Pp]lease wait", r"[Ww]arning", r"[Ee]nter.*username",
+                 r"[Ee]nter.*password", r"[Cc]onnection timed out", prompt,
+                 r"Escape character is.*"],
+                timeout=timeout, internal_timeout=0.5)
+            output += text
+            if match == 0:  # "Are you sure you want to continue connecting"
+                if debug:
+                    logging.debug("Got 'Are you sure...', sending 'yes'")
+                session.sendline("yes")
+                continue
+            elif match in [1, 2, 3, 10]:  # "password:"
+                if password_prompt_count == 0:
+                    if debug:
+                        logging.debug("Got password prompt, sending '%s'",
+                                      password)
+                    session.sendline(password)
+                    password_prompt_count += 1
+                    continue
+                else:
+                    raise Exception("Got password prompt twice", text)
+            elif match == 4 or match == 9:  # "login:"
+                if login_prompt_count == 0 and password_prompt_count == 0:
+                    if debug:
+                        logging.debug("Got username prompt; sending '%s'",
+                                      username)
+                    session.sendline(username)
+                    login_prompt_count += 1
+                    continue
+                else:
+                    if login_prompt_count > 0:
+                        msg = "Got username prompt twice"
+                    else:
+                        msg = "Got username prompt after password prompt"
+                    raise Exception(msg, text)
+            elif match == 5:  # "Connection closed"
+                raise Exception("Client said 'connection closed'", text)
+            elif match == 6:  # "Connection refused"
+                raise Exception("Client said 'connection refused'", text)
+            elif match == 11:  # Connection timeout
+                raise Exception("Client said 'connection timeout'", text)
+            elif match == 7:  # "Please wait"
+                if debug:
+                    logging.debug("Got 'Please wait'")
+                timeout = 30
+                continue
+            elif match == 8:  # "Warning added RSA"
+                if debug:
+                    logging.debug("Got 'Warning added RSA to known host list")
+                continue
+            elif match == 12:  # prompt
+                if debug:
+                    logging.debug("Got shell prompt -- logged in")
+                break
+            elif match == 13:  # console prompt
+                logging.debug("Got console prompt, send return to show login")
+                session.sendline()
+        except aexpect.ExpectTimeoutError, e:
+            raise Exception(e.output)
+        except aexpect.ExpectProcessTerminatedError, e:
+            raise Exception(e.status, e.output)
+
+    return output
+
+
+def remote_login(client, host, port, username, password, prompt, linesep="\n",
+                 log_filename=None, timeout=10, interface=None,
+                 status_test_command="echo $?", verbose=False, use_key=False):
+    """
+    Log into a remote host (guest) using SSH/Telnet/Netcat.
+
+    :param client: The client to use ('ssh', 'telnet' or 'nc')
+    :param host: Hostname or IP address
+    :param port: Port to connect to
+    :param username: Username (if required)
+    :param password: Password (if required)
+    :param prompt: Shell prompt (regular expression)
+    :param linesep: The line separator to use when sending lines
+            (e.g. '\\n' or '\\r\\n')
+    :param log_filename: If specified, log all output to this file
+    :param timeout: The maximal time duration (in seconds) to wait for
+            each step of the login procedure (i.e. the "Are you sure" prompt
+            or the password prompt)
+    :interface: The interface the neighbours attach to (only use when using ipv6
+                linklocal address.)
+    :param status_test_command: Command to be used for getting the last
+            exit status of commands run inside the shell (used by
+            cmd_status_output() and friends).
+
+    :raise LoginError: If using ipv6 linklocal but not assign a interface that
+                       the neighbour attache
+    :raise LoginBadClientError: If an unknown client is requested
+    :raise: Whatever handle_prompts() raises
+    :return: A ShellSession object.
+    """
+    if host and host.lower().startswith("fe80"):
+        if not interface:
+            raise Exception("When using ipv6 linklocal an interface must "
+                            "be assigned")
+        host = "%s%%%s" % (host, interface)
+
+    verbose = verbose and "-vv" or ""
+    if client == "ssh":
+        if not use_key:
+            cmd = ("ssh %s -o UserKnownHostsFile=/dev/null "
+                   "-o StrictHostKeyChecking=no "
+                   "-o PreferredAuthentications=password -p %s %s@%s" %
+                   (verbose, port, username, host))
+        else:
+            cmd = ("ssh %s -o UserKnownHostsFile=/dev/null "
+                   "-o StrictHostKeyChecking=no "
+                   "-p %s %s@%s" %
+                   (verbose, port, username, host))
+    elif client == "telnet":
+        cmd = "telnet -l %s %s %s" % (username, host, port)
+    elif client == "nc":
+        cmd = "nc %s %s %s" % (verbose, host, port)
+    else:
+        raise Exception(client)
+
+    if verbose:
+        logging.debug("Login command: '%s'", cmd)
+    session = aexpect.ShellSession(cmd, linesep=linesep, prompt=prompt,
+                                   status_test_command=status_test_command)
+    if use_key and not password:
+        password = ""
+
+    try:
+        handle_prompts(session, username, password, prompt, timeout)
+    except Exception:
+        session.close()
+        raise
+
+    return session
+
+
+def wait_for_login(client, host, port, username, password, prompt,
+                   linesep="\n", log_filename=None, timeout=240,
+                   internal_timeout=10, interface=None, use_key=False):
+    """
+    Make multiple attempts to log into a guest until one succeeds or timeouts.
+
+    :param timeout: Total time duration to wait for a successful login
+    :param internal_timeout: The maximum time duration (in seconds) to wait for
+                             each step of the login procedure (e.g. the
+                             "Are you sure" prompt or the password prompt)
+    :interface: The interface the neighbours attach to (only use when using ipv6
+                linklocal address.)
+    :see: remote_login()
+    :raise: Whatever remote_login() raises
+    :return: A ShellSession object.
+    """
+    logging.debug("Attempting to log into %s:%s using %s (timeout %ds)",
+                  host, port, client, timeout)
+    end_time = time.time() + timeout
+    verbose = False
+    while time.time() < end_time:
+        try:
+            return remote_login(client, host, port, username, password, prompt,
+                                linesep, log_filename, internal_timeout,
+                                interface, verbose=verbose, use_key=use_key)
+        except Exception, e:
+            logging.debug(e)
+            verbose = True
+        time.sleep(2)
+    # Timeout expired; try one more time but don't catch exceptions
+    return remote_login(client, host, port, username, password, prompt,
+                        linesep, log_filename, internal_timeout, interface,
+                        use_key=use_key)
+
+
+class RemoteRunner(object):
+    """
+    Class to provide a utils.run-like method to execute command on
+    remote host or guest. Provide a similar interface with utils.run
+    on local.
+    """
+
+    def __init__(self, client="ssh", host=None, port="22", username="root",
+                 password=None, prompt=r"[\#\$]\s*$", linesep="\n",
+                 log_filename=None, timeout=240, internal_timeout=10,
+                 session=None, use_key=False):
+        """
+        Initialization of RemoteRunner. Init a session login to remote host or
+        guest.
+
+        :param client: The client to use ('ssh', 'telnet' or 'nc')
+        :param host: Hostname or IP address
+        :param port: Port to connect to
+        :param username: Username (if required)
+        :param password: Password (if required)
+        :param prompt: Shell prompt (regular expression)
+        :param linesep: The line separator to use when sending lines
+                (e.g. '\\n' or '\\r\\n')
+        :param log_filename: If specified, log all output to this file
+        :param timeout: Total time duration to wait for a successful login
+        :param internal_timeout: The maximal time duration (in seconds) to wait
+                for each step of the login procedure (e.g. the "Are you sure"
+                prompt or the password prompt)
+        :param session: An existing session
+        :see: wait_for_login()
+        :raise: Whatever wait_for_login() raises
+        """
+        self.host = host
+        self.username = username
+        self.password = password
+        if session is None:
+            if host is None:
+                raise Exception("Neither host, nor session was defined!")
+            self.session = wait_for_login(client, host, port, username,
+                                          password, prompt, linesep,
+                                          log_filename, timeout,
+                                          internal_timeout, use_key=use_key)
+        else:
+            self.session = session
+        # Init stdout pipe and stderr pipe.
+        random_pipe = random.randint(1, 10)
+        self.stdout_pipe = '/tmp/cmd_stdout_%s' % random_pipe
+        self.stderr_pipe = '/tmp/cmd_stderr_%s' % random_pipe
+
+    def run(self, command, timeout=60, ignore_status=False,
+            internal_timeout=None):
+        """
+        Method to provide a utils.run-like interface to execute command on
+        remote host or guest.
+
+        :param timeout: Total time duration to wait for command return.
+        :param ignore_status: If ignore_status=True, do not raise an exception,
+                              no matter what the exit code of the command is.
+                              Else, raise CmdError if exit code of command is not
+                              zero.
+        """
+        # Redirect the stdout and stderr to file, Deviding error message
+        # from output, and taking off the color of output. To return the same
+        # result with utils.run() function.
+        command = "%s 1>%s 2>%s" % (
+            command, self.stdout_pipe, self.stderr_pipe)
+        status, _ = self.session.cmd_status_output(command, timeout=timeout,
+                                                   internal_timeout=internal_timeout)
+        output = self.session.cmd_output("cat %s;rm -f %s" %
+                                         (self.stdout_pipe, self.stdout_pipe))
+        errput = self.session.cmd_output("cat %s;rm -f %s" %
+                                         (self.stderr_pipe, self.stderr_pipe))
+        cmd_result = CmdResult(command=command, exit_status=status,
+                               stdout=output, stderr=errput)
+        if status and (not ignore_status):
+            raise CmdError(command, cmd_result)
+        return cmd_result
+
+
+class CmdError(Exception):
+    def __init__(self, command=None, result=None, additional_text=None):
+        self.command = command
+        self.result = result
+        self.additional_text = additional_text
+
+    def __str__(self):
+        if self.result is not None:
+            if self.result.interrupted:
+                msg = "Command '%s' interrupted by %s"
+                msg %= (self.command, self.result.interrupted)
+            elif self.result.exit_status is None:
+                msg = "Command '%s' failed and is not responding to signals"
+                msg %= self.command
+            else:
+                msg = "Command '%s' failed (rc=%d)"
+                msg %= (self.command, self.result.exit_status)
+            if self.additional_text:
+                msg += ", " + self.additional_text
+            return msg
+        else:
+            return "CmdError"
+
+
+class CmdResult(object):
+    """
+    Command execution result.
+
+    :param command: String containing the command line itself
+    :param exit_status: Integer exit code of the process
+    :param stdout: String containing stdout of the process
+    :param stderr: String containing stderr of the process
+    :param duration: Elapsed wall clock time running the process
+    :param pid: ID of the process
+    """
+
+    def __init__(self, command="", stdout="", stderr="",
+                 exit_status=None, duration=0, pid=None):
+        self.command = command
+        self.exit_status = exit_status
+        self.stdout = stdout
+        self.stderr = stderr
+        self.duration = duration
+        self.interrupted = False
+        self.pid = pid
+
+    def __repr__(self):
+        cmd_rep = ("Command: %s\n"
+                   "Exit status: %s\n"
+                   "Duration: %s\n"
+                   "Stdout:\n%s\n"
+                   "Stderr:\n%s\n"
+                   "PID:\n%s\n" % (self.command, self.exit_status,
+                                   self.duration, self.stdout, self.stderr,
+                                   self.pid))
+        if self.interrupted:
+            cmd_rep += "Command interrupted by %s\n" % self.interrupted
+        return cmd_rep
